c8728d96267fa8210bed7c06358b645b4670ec44
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-05 12:22:30 +0200

    Commit

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,13 @@
+public class Triangle {
+    int width;
+    int height;
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+}
